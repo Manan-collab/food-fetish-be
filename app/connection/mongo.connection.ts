67704e1ba0d6eb99c1f9e61509046e3913a664c3
Manan-collab <mananmer9769@gmail.com>
2024-07-12 14:02:15 +0530

    refactor: mongo connection

--- app/connection/mongo.connection.ts
+++ app/connection/mongo.connection.ts
@@ -2,11 +2,7 @@ import { connect, ConnectOptions } from "mongoose";
 
 export const connectToMongo = async (): Promise<string> => {
   try {
-    await connect(process.env.MONGO_CONNECTION_STRING as string, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    } as ConnectOptions);
-
+    await connect(process.env.MONGO_CONNECTION_STRING||'');
     console.log("Connected to MongoDB");
     return "Connected to MongoDB";
   } catch (error) {
